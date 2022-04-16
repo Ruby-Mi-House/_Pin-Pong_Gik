0b81a51db8d765775ac5f01a76e42a5145e14b90
Ruby-Mi-House <102861462+Ruby-Mi-House@users.noreply.github.com>
2022-04-16 15:49:17 +0300

    Update Pin-Pong.py

--- Pin-Pong.py
+++ Pin-Pong.py
@@ -15,14 +15,14 @@ clock.tick(FPS)
 speed = 5
 mixer.music.load("Fonam (1).mp3")
 mixer.music.play()
-#kick = mixer.Sound("Stolknovenie.mp3")
+kick = mixer.Sound("Stolknovenie.ogg")
 font.init()
-#font = font.SysFont("Times New Roman",70)
+font = font.SysFont("Times New Roman",70)
 finish = False
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (365,890))
+        self.image = transform.scale(image.load(player_image), (50,50))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -62,29 +62,28 @@ class Boll(GameSprite):
     def Update(self):
         global direction
         global direction_1
-        if self.rect.x <= 20 :
+        if self.rect.x <= 0:
             direction = "right"
-        if self.rect.x >= 970:
+        if self.rect.x >= 1050:
             direction = "left"
         if direction == "left":
             self.rect.x -= self.speed
-        else:
+        if direction == "right":
             self.rect.x += self.speed
 
         if self.rect.y <= 5:
             direction_1 = "down"
-        if self.rect.y >= 710:
+        if self.rect.y >= 690:
             direction_1 = "up"
         if direction_1 == "up":
             self.rect.y -= self.speed
-        else:
+        if direction_1 == "down":
             self.rect.y += self.speed
 a_1 = Player_1('P_1.png', 30, 430, speed)
 a_2 = Player_2('P_2.png', 880, 430, speed)
-boll = Boll("Boll.png", randint(35,620), randint(35,620),speed)
+boll = Boll("Boll.png", randint(300,500), randint(100,560),speed)
 while game:
-    #sprites_list = sprite.spritecollide(a_1, boll, True)
-    #sprites_list = sprite.spritecollide(a_2, boll, True)
+    win = font.render("Игра окончена!", True,(255,215,0))
     window.blit(background, (0,0))
     window.blit(a_1.image,(a_1.rect.x,a_1.rect.y))
     window.blit(a_2.image,(a_2.rect.x,a_2.rect.y))
@@ -92,6 +91,17 @@ while game:
     a_1.Update()
     a_2.Update()
     boll.Update()
+    if sprite.collide_rect(a_1, boll):
+        direction = "right"
+    if sprite.collide_rect(a_2, boll):
+        direction = "left"
+    if boll.rect.x < 60 or boll.rect.x > 990:
+        window.blit(win,(262,375))
+        kick.play()
+        sleep(5) 
+        window.blit(win,(262,375))
+        finish = True   
+        game = False
     for e in event.get():
         if e.type == QUIT:
             game = False
